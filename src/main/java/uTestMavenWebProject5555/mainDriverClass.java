505ae4e5a803e94080a6e371d8120a3cb6635b1d
Behera, Swastik <swastik.behera@pwc.com>
2018-12-05 08:23:37 +0530

    1st commit

---
+++ src/main/java/uTestMavenWebProject5555/mainDriverClass.java
@@ -0,0 +1,23 @@
+package uTestMavenWebProject5555;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class mainDriverClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+		capabilities.setCapability(InternetExplorerDriver.FORCE_CREATE_PROCESS, true);
+		capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		System.setProperty("webdriver.ie.driver","C:\\Users\\swastikbvst\\Desktop\\New folder\\Selenium\\IEDriver\\IEDriverServer.exe");
+		WebDriver driver = new InternetExplorerDriver(capabilities);		
+		driver.get("http://localhost:80/");
+		System.out.println("Navigate to Url method executed");
+		String text = driver.findElement(By.tagName("h2")).getText();
+		System.out.println(text);
+
+	}
+
+}
